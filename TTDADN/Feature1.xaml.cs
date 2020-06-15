0dbfb97166f140fc024066836ff8f9e02068258c
Thong Nguyen <thongnguyen050999@gmail.com>
2020-06-15 21:55:38 +0700

    Mother fucker

--- TTDADN/Feature1.xaml.cs
+++ TTDADN/Feature1.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,11 +21,69 @@ namespace TTDADN
     /// <summary>
     /// Interaction logic for Feature1.xaml
     /// </summary>
-    public partial class Feature1 : Window
+    /// 
+    public class Parameter : INotifyPropertyChanged
     {
+        private int _temperature;
+
+        public int Temperature
+        {
+            get { return _temperature; }
+            set { _temperature = value; OnPropertyChanged(); }
+        }
+
+        private int _light;
+
+        public int Light
+        {
+            get { return _light; }
+            set { _light = value; OnPropertyChanged(); }
+        }
+
+        private int _humidity;
+
+        public int Humidity
+        {
+            get { return _humidity; }
+            set { _humidity = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public partial class Feature1 : Window, INotifyPropertyChanged
+    {
+        private Parameter _param;
+
+        public Parameter Param
+        {
+            get { return _param; }
+            set { _param = value; OnPropertyChanged(); }
+        }
+
+
         public Feature1()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var jsonString = File.ReadAllText("D:\\Projects\\C#\\TTDADN_official\\TTDADN\\bin\\Debug\\demo.json");
+            Param = JsonConvert.DeserializeObject<Parameter>(jsonString);
+        }
+
     }
 }
--- TTDADN/Feature3.xaml.cs
+++ TTDADN/Feature3.xaml.cs
@@ -12,17 +12,62 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace TTDADN
 {
     /// <summary>
     /// Interaction logic for Feature3.xaml
     /// </summary>
-    public partial class Feature3 : Window
+    //public class Light
+    //{
+    //    public int lightValue;
+
+    //    public int LightValue
+    //    {
+    //        get { return lightValue; }
+    //        set { lightValue = value; } 
+    //    }
+    //}
+
+    public class PublishParameter : INotifyPropertyChanged
     {
-        static public string script = "python publisher.py --topic \"abc\" --state 1";
-        public static void run_cmd_publisher()
+        private int _light;
+
+        public int Light
         {
+            get { return _light; }
+            set { _light = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+    public partial class Feature3 : Window, INotifyPropertyChanged
+    {
+        private PublishParameter param;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PublishParameter Param
+        {
+            get { return param; }
+            set { param = value; OnPropertyChanged(); }
+        }
+
+        static public string script = "python publisher.py --topic \"abc\" --state ";
+        public void run_cmd_publisher()
+        {
+            script += Param.Light.ToString();
             Process process = new Process();
             process.StartInfo.FileName = "cmd.exe";
             process.StartInfo.CreateNoWindow = true;
@@ -39,13 +84,27 @@ namespace TTDADN
             process.StandardInput.WriteLine(script);
             process.StandardInput.Flush();
             process.StandardInput.Close();
+            MessageBox.Show("Finished publishing temperature");
             //process.WaitForExit();
 
         }
         public Feature3()
         {
-            run_cmd_publisher();
             InitializeComponent();
+            this.DataContext = this;
+            Param = new PublishParameter();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            run_cmd_publisher();
+            Param.Light = 0;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Param.Light = 0;
+            run_cmd_publisher();
         }
     }
 }
